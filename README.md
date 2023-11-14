620036d152c1154484af6701975267d4fc695dc6
Glenn Ou <[glennou2@gmail.com]>
2023-11-14 14:17:10 +0400

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+## Graph QL qucik Demo learning:
+
+### 1.Simple Server Build
+[Start](https://github.com/GlennOu66304/Full-Stack-Development/blob/9c6a9e52cbd32e6af72b5a6c0e45149b9610b41a/Nodejs/Graph%20QL.md#L1)
--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const { gql, ApolloServer } = require("apollo-server");
+
+// Define the User type
 const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+  }
+
   type Query {
     greeting: String
   }
+
+  type Mutation {
+    registerUser(username: String!, email: String!): User!
+  }
 `;
 
+// Define the resolvers for the User type and createUser mutation
 const resolvers = {
   Query: {
-    greeting: () => "Hello word!",
+    greeting: () => "Hello world!",
+  },
+  Mutation: {
+    registerUser: (parent, args) => {
+      // Here you would typically create a new user in your database
+      // and return the newly created user object
+      const newUser = {
+        id: "1",
+        username: args.username,
+        email: args.email,
+      };
+      return newUser;
+    },
   },
 };
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server
   .listen({ port: 8000 })
-  .then(({ url }) => console.log(`server is running at${url}`));
+  .then(({ url }) => console.log(`server is running at ${url}`));
